838f77dabb0b32976040551a698748dbca8fd2c9
fyx <info@yfyx.me>
2025-04-14 13:35:33 +0700

    Updates footer logo text

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="w-full" aria-label="Footer">
       <div className="mx-auto flex flex-col items-center justify-center">
         <div className="select-none bg-gradient-to-r from-zinc-200/20 via-zinc-400/10 to-zinc-200/20 bg-clip-text text-[8rem] font-extrabold tracking-tighter text-transparent dark:from-zinc-800/20 dark:via-zinc-700/20 dark:to-zinc-800/20">
-          yfyx
+          UG-PULSE
         </div>
       </div>
     </footer>
